d3dc77b81a07cec4239560d74dbf9bd97b94ce70
yohmod <teguhsahestu10@gmail.com>
2022-08-21 18:12:05 +0700

    upload file

---
+++ service.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+insmod /system/lib/modules/88XXau.ko
+insmod /system/lib/modules/8188eu.ko
+insmod /system/lib/modules/8814au.ko
+insmod /system/lib/modules/gspca_main.ko
+insmod /system/lib/modules/r8188eu.ko
+insmod /system/lib/modules/r8712u.ko
+insmod /system/lib/modules/r8723bs.ko
